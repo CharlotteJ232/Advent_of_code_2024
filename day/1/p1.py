571ed4c4418192177a75abad40c40ecf6ac4bb7a
Charlotte <charlotte-jansen@hotmail.com>
2024-12-03 11:05:25 +0100

    initial commit

---
+++ day/1/p1.py
@@ -0,0 +1,11 @@
+# Code for part 1
+
+from pathlib import Path
+
+
+def main():
+    print("empty")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day/1/test_p1.py
@@ -0,0 +1,10 @@
+# from p1 import
+from pathlib import Path
+
+
+# Tests need to start with test_ or end with _test, as well as the file they are in, for pytest to automatically recognize them (by default)
+# Test name: Given, When, Then
+# def test_given_a_string_when_the_only_digits_are_in_the_first_and_last_place_combine_them():
+#     # Structure: Arrange, Act, Assert
+#     # Arrange
+#     line = "1abc2"
